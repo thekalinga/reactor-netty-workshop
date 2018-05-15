898bf5cfdcf8e5840d4e94eed01be782278ab985
Violeta Georgieva <vgeorgieva@pivotal.io>
2018-05-15 13:24:36 +0300

    [TcpSendFileTests] Configure TCP Client

--- src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
+++ src/test/java/io/spring/workshop/reactornetty/tcp/TcpSendFileTests.java
@@ -1,5 +1,7 @@
 package io.spring.workshop.reactornetty.tcp;
 
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.junit.Test;
 import reactor.core.publisher.Mono;
 import reactor.netty.Connection;
@@ -23,7 +25,7 @@ import static org.junit.Assert.assertNotNull;
 public class TcpSendFileTests {
 
     @Test
-    public void sendFileTest() {
+    public void sendFileTest() throws Exception {
         DisposableServer server =
                 TcpServer.create()   // Prepares a TCP server for configuration.
                          .port(0)    // Configures the port number as zero, this will let the system pick up
@@ -51,6 +53,11 @@ public class TcpSendFileTests {
                 TcpClient.create()            // Prepares a TCP client for configuration.
                          .port(server.port()) // Obtains the server's port and provide it as a port to which this
                                               // client should connect.
+                         // Configures SSL providing an already configured SslContext.
+                         .secure(SslContextBuilder.forClient()
+                                                  .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                                                  .build())
+                         .wiretap()           // Applies a wire logger configuration.
                          .connectNow();       // Blocks the client and returns a Connection.
 
         assertNotNull(client);
